b879adc3d4c7ac97cb50da0351cfc84f340d8b0a
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2025-11-24 23:45:28 +0530

    trying out L2 distance function between vectors

--- packages/way-engine/src/core.cc
+++ packages/way-engine/src/core.cc
@@ -1,4 +1,5 @@
 #include "core.h"
+#include <vector>
 
 double sum_array(const double* data, size_t length) {
     double sum = 0.0;
@@ -7,3 +8,42 @@ double sum_array(const double* data, size_t length) {
     }
     return sum;
 }
+
+struct VectorDB {
+    std::vector<float> data;
+    int dim;
+
+    VectorDB(int dim): dim(dim) {}
+
+    void add(const std::vector<float> &v){
+        for (float x : v) {
+            data.push_back(x);
+        }
+    }
+
+    // distance between two vectors is just normal distance formula!
+    float l2(const float* a, const float* b, int dim) {
+        float d = 0;
+        for (int i = 0; i < dim; i++) {
+            float diff = a[i] - b[i];
+            d += diff * diff;
+        }
+        return d;
+    }
+
+    int search(const std::vector<float>& query) {
+        float bestDist = 1e30;
+        int bestIndex = -1;
+
+        for (int i = 0; i < data.size(); i += dim) {
+            float d = l2(&data[i], query.data(), dim);
+
+            if (d < bestDist) {
+                bestDist = d;
+                bestIndex = i / dim;
+            }
+        }
+        return bestIndex;
+    }
+};
+
