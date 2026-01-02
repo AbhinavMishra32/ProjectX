a6038c6a0eb3f7a017381a018f1cf85dd7c62db3
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2026-01-02 10:00:00 +0530

    Configure Next.js for native addons and add OpenAI dependencies

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {},
+  experimental: {
+    serverComponentsExternalPackages: ['@worldview/way-engine'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@worldview/way-engine': 'commonjs @worldview/way-engine'
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
